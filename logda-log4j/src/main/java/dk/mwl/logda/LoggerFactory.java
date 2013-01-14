15d18abb00bb4ab47b368142339d7007b634a18e
Martin Westergaard Lassen <martin@mwl.dk>
2013-01-14 22:07:33 +0100

    Streamlined factory with Log4j

--- logda-log4j/src/main/java/dk/mwl/logda/LoggerFactory.java
+++ logda-log4j/src/main/java/dk/mwl/logda/LoggerFactory.java
@@ -3,11 +3,11 @@ package dk.mwl.logda;
 import dk.mwl.logda.log4j.Log4jLogger;
 
 public class LoggerFactory {
-    public static Logger create(String type) {
+    public static Logger getLogger(String type) {
         return new Log4jLogger(type);
     }
 
-    public static Logger create(Class clazz) {
+    public static Logger getLogger(Class clazz) {
         return new Log4jLogger(clazz);
     }
 }
